1ca7b6a8a191529ca2f9d5ea1e0ec2689fd3f568
Giovanna <radica.giovanna@gmail.com>
2022-05-24 22:56:44 +0200

    feat: validation errors

--- src/shared/components/form/Form.tsx
+++ src/shared/components/form/Form.tsx
@@ -5,12 +5,13 @@ import { SubmitButton } from "../button/SubmitButton";
 import Input from "../input";
 import { Select } from "../select/Select";
 import "./Form.css";
+import { inputs } from "./inputs";
 
 export const Form: FC = () => {
   const [invoiceData, setInvoiceData] = useState<InvoiceDataType>();
   const [companyName, setCompanyName] = useState<string>("Apophis_Contacts");
 
-  const { errors, createInvoice, invoiceLink } = useInvoice();
+  const { createInvoice, invoiceLink, errors } = useInvoice();
 
   const handleChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -24,6 +25,7 @@ export const Form: FC = () => {
     },
     []
   );
+
   const handleSelect = ({
     currentTarget: { value },
   }: React.ChangeEvent<HTMLSelectElement>) => setCompanyName(value);
@@ -31,30 +33,17 @@ export const Form: FC = () => {
   return (
     <div className="form">
       <Select handleChange={handleSelect} />
-      <Input
-        label="Numero fattura"
-        name="invoiceNum"
-        placeholder="Inserisci numero"
-        handleChange={handleChange}
-      />
-      <Input
-        label="Data compilazione"
-        name="date"
-        placeholder="Inserisci data"
-        handleChange={handleChange}
-      />
-      <Input
-        label="Mese di riferimento"
-        name="currentMonth"
-        placeholder="Inserisci mese"
-        handleChange={handleChange}
-      />
-      <Input
-        label="Compenso netto"
-        name="netAmount"
-        placeholder="Inserisci compenso"
-        handleChange={handleChange}
-      />
+      {inputs.map(({ key, label, placeholder }) => {
+        return (
+          <Input
+            label={label}
+            name={key}
+            placeholder={placeholder}
+            handleChange={handleChange}
+            error={errors?.[key]}
+          />
+        );
+      })}
       <SubmitButton onClick={() => createInvoice(companyName, invoiceData)} />
       {invoiceLink ? <a href={invoiceLink}>Scarica Fattura</a> : null}
     </div>
---
+++ src/shared/components/form/inputs.ts
@@ -0,0 +1,18 @@
+export const inputs = [
+  {
+    key: "invoiceNum",
+    label: "Numero fattura",
+    placeholder: "Inserisci numero",
+  },
+  { key: "date", label: "Data compilazione", placeholder: "Inserisci data" },
+  {
+    key: "currentMonth",
+    label: "Mese di riferimento",
+    placeholder: "Inserisci mese",
+  },
+  {
+    key: "netAmount",
+    label: "Compenso netto",
+    placeholder: "Inserisci compenso",
+  },
+];
